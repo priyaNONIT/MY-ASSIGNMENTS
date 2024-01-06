ce92f8cd30b9a7d3479539cca15c72f6095b9169
priyaNONIT <155842085+priyaNONIT@users.noreply.github.com>
2024-01-06 23:37:32 +0530

    Add files via upload
    DAY1 HOME ASSIGNMENT

---
+++ Firefoxx.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefoxx {
+
+	public static void main(String[] args) {
+		String browserName="fireFoxx";
+				float browserVersion=202.1f;
+				int noOfBrowser=8;
+				char browserLogo='F';
+				boolean pageLoaded=true;
+				System.out.println("Browser Name:"+browserName);
+				System.out.println("Browser Version:"+browserVersion);
+				System.out.println("Number Of Browser:"+noOfBrowser);
+				System.out.println("Browser Logo:"+browserLogo);
+				System.out.println("PageLoaded:"+pageLoaded);
+				
+				
+
+	}
+
+}
